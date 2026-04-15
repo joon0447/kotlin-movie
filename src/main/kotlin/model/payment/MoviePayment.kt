b8d58798281f86a3e9fdee9f09704d4c5701fdd0
joon0447 <joon0447@naver.com>
2026-04-15 16:38:50 +0900

    refactor: MoviePayment의 reservations 가시성 private 으로 변경

--- src/main/kotlin/model/payment/MoviePayment.kt
+++ src/main/kotlin/model/payment/MoviePayment.kt
@@ -5,7 +5,7 @@ import model.payment.policy.price.PricingPolicy
 import model.reservation.MovieReservationResult
 
 class MoviePayment(
-    val reservations: List<MovieReservationResult.Success>,
+    private val reservations: List<MovieReservationResult.Success>,
     private val policies: List<DiscountPolicy>,
 ) {
     val originalPrice: Int = reservations.sumOf { PricingPolicy.price(it.seat.grade) }
